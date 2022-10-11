dcd01fd16a5867a89fc98771e73e9c7ecd7658cc
KarinaShtankova <shtankovads@gmail.com>
2022-10-11 18:05:56 +0300

    adds files and finish first task

---
+++ js/01-gallery.js
@@ -0,0 +1,53 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryRef = document.querySelector(".gallery");
+
+const markup = galleryItems
+	.map(
+		({ preview, original, description }) => `
+
+    <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`,
+	)
+	.join("");
+
+galleryRef.insertAdjacentHTML("beforeend", markup);
+// console.log(markup);
+
+galleryRef.addEventListener("click", onGalleryClick);
+
+function onGalleryClick(evt) {
+	evt.preventDefault();
+	const modalImage = evt.target.getAttribute(`data-source`);
+	// console.log(`Зображення`, modalImage);
+
+	const instance = basicLightbox.create(`
+    <img src="${modalImage}" >
+`);
+
+	instance.show();
+
+	window.addEventListener(`keydown`, onEscKeyPress);
+
+	function onEscKeyPress(evt) {
+		// console.log(evt);
+		// console.log(evt.key);
+		if (evt.key === "Escape") {
+			instance.close();
+		}
+	}
+
+	// console.log(`event.target:`, evt.target);
+	// console.log(`event.currentTarget:`, evt.currentTarget)
+}
